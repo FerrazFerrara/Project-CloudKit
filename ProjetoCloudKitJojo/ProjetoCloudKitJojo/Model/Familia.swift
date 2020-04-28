7128b5d86256d3ae7b44f5a6b7804cdf57c3b784
JonatasCoutinho <jonatascoutinho10@gmail.com>
2020-04-28 17:34:10 -0300

    estrutura inicial

---
+++ ProjetoCloudKitJojo/ProjetoCloudKitJojo/Model/Familia.swift
@@ -0,0 +1,13 @@
+//
+//  Familia.swift
+//  ProjetoCloudKitJojo
+//
+//  Created by Jonatas Coutinho de Faria on 28/04/20.
+//  Copyright © 2020 Ferraz. All rights reserved.
+//
+
+import Foundation
+
+class Familia{
+    
+}
