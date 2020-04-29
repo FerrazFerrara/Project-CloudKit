dcf403fd094e02c0669282a989a8e10360852a39
JonatasCoutinho <jonatascoutinho10@gmail.com>
2020-04-29 17:36:52 -0300

    arrumando sets do CRUD de usuarios

--- ProjetoCloudKitJojo/ProjetoCloudKitJojo/Model/Usuario.swift
+++ ProjetoCloudKitJojo/ProjetoCloudKitJojo/Model/Usuario.swift
@@ -33,7 +33,7 @@ class Usuario{
         self.derrota = derrota
     }
     
-    init(recordID: CKRecord.ID, idFamilia: String, nome: String, pontuacao: Int64, foto: CKAsset, conquista: [Int64], vitoria: Int64, derrota: Int64) {
+    init(recordID: CKRecord.ID, idFamilia: NSString, nome: NSString, pontuacao: NSNumber, foto: CKAsset, conquista: [NSNumber], vitoria: NSNumber, derrota: NSNumber) {
         self.recordID = recordID
         setIdFamilia(idFamilia: idFamilia)
         setNome(nome: nome)
@@ -48,31 +48,43 @@ class Usuario{
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setIdFamilia(idFamilia: String){
-        self.idFamilia = String()
+    private func setIdFamilia(idFamilia: NSString){
+        self.idFamilia = idFamilia as String
     }
     
-    private func setNome(nome: String){
-        self.nome = String()
+    private func setNome(nome: NSString){
+        self.nome = nome as String
     }
     
-    private func setPontuacao(pontuacao: Int64){
-        self.pontuacao = Int()
+    private func setPontuacao(pontuacao: NSNumber){
+        self.pontuacao = pontuacao.intValue
     }
     
     private func setFoto(foto: CKAsset){
-        self.foto = UIImage()
+        
+        if let data = NSData(contentsOf: foto.fileURL!) {
+            self.foto = UIImage(data: data as Data)
+        }else{
+            //Foto padrao
+             self.foto = UIImage()
+        }
     }
     
-    private func setConquista(conquista: [Int64]){
-        self.conquista = [Bool]()
+    private func setConquista(conquista: [NSNumber]){
+        var aux: [Bool] = []
+        
+        for i in conquista{
+            aux.append(i.boolValue)
+        }
+        
+        self.conquista = aux
     }
     
-    private func setVitoria(vitoria: Int64){
-        self.derrota = Int()
+    private func setVitoria(vitoria: NSNumber){
+        self.derrota = vitoria.intValue
     }
     
-    private func setDerrota(derrota: Int64){
-        self.vitoria = Int()
+    private func setDerrota(derrota: NSNumber){
+        self.vitoria = derrota.intValue
     }
 }
