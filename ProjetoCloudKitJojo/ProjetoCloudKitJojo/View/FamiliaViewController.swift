0b545cfc994a81a0e090b4cd5ee109b59059fe25
Gabriel Ferraz Fontes <brielff@hotmail.com>
2020-04-30 16:36:31 -0300

    familia viewcontroller adicionada ao projeto

---
+++ ProjetoCloudKitJojo/ProjetoCloudKitJojo/View/FamiliaViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FamiliaViewController.swift
+//  ProjetoCloudKitJojo
+//
+//  Created by Ferraz on 30/04/20.
+//  Copyright © 2020 Ferraz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FamiliaViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
